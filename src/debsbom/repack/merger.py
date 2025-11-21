ssnaaz/debsbom

commit 6c5458162a9c1194f185fe855f35ced822214efb

chore(repack): create tar with stable order and owner
As a preparation to repack reproducible, we pin the owner and sorting of
the files within the tar.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/debsbom/repack/merger.py
@@ -125,9 +125,18 @@ class SourceArchiveMerger:
             # repack archive
             sources = [s.name for s in Path(tmpdir).iterdir() if s.is_dir() or s.is_file()]
             tmpfile = merged.with_suffix(f"{merged.suffix}.tmp")
+            # options to build tar reproducible
+            repro_tar_opts = [
+                "--force-local",
+                "--format=gnu",
+                "--sort=name",
+                "--owner=0",
+                "--group=0",
+                "--numeric-owner",
+            ]
             with open(tmpfile, "wb") as outfile:
                 tar_writer = subprocess.Popen(
-                    ["tar", "c"] + sorted(sources),
+                    ["tar", "c"] + repro_tar_opts + sorted(sources),
                     stdout=subprocess.PIPE,
                     cwd=tmpdir,
                 )

commit 6a2b340ba5536fd0307fb12d9d7cf86679ca7508

fix(repack): enforce mode=755 to avoid permission discrepancies
Archives created on different systems had varying file permissions,
leading to different checksums after repacking. Setting mode=755
to normalize permissions to ensure reproducibility.

Fixes: 6c54581 (chore(repack): create tar with stable order and owner)

Signed-off-by: Syeda Shagufta Naaz <syedashagufta.naaz@siemens.com>

src/debsbom/repack/merger.py
@@ -185,6 +185,7 @@ class SourceArchiveMerger:
                 "--force-local",
                 "--format=gnu",
                 "--sort=name",
+                "--mode=755",
                 "--owner=0",
                 "--group=0",
                 "--numeric-owner",